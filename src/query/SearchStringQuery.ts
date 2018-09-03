1ba531f6b82a955ef11678fb597eb355e7cb4ca7
raddecke <ruben.dedecker@ugent.be>
2018-09-03 12:13:03 +0200

    pulling

--- src/query/SearchStringQuery.ts
+++ src/query/SearchStringQuery.ts
@@ -7,22 +7,29 @@ import ChildRelation from '../tree/ChildRelation';
 import OrCondition from '../condition/OrCondition';
 import StringContainedCondition from '../condition/StringContainedCondition';
 import StringContainedSaveCondition from '../condition/StringContainedSaveCondition';
+import Query from './Query';
 
-export default class SearchStringQuery extends SingleQuery{
-    followcondition: Condition;
+export default class SearchStringQuery extends Query{
+
+    query(): Promise<Session> {
+        throw new Error("Method not implemented.");
+    }
+    followCondition: Condition;
 
     constructor(session:Session,
         searchstring: string)
         {
             //TODO:: FIX THIS, NOT CORRECT, DONE TO STOP TYPING ERRORS
-        super(new StringContainedSaveCondition(), new OrCondition(new StringContainsCondition(), new StringContainedCondition()));
-        super.set_iteration_value(searchstring);
-        super.set_iteration_action(function(node: Node, relation: ChildRelation, child: Node, iterationValue){
-            if (child.getValue().length < searchstring.length){ 
-                return iterationValue.slice(child.getValue().length) 
-                }
-                return "";
-            })
-        }
+        super()
+        this.followCondition = new OrCondition(new StringContainsCondition(), new StringContainedCondition());
+        // super.set_iteration_value(searchstring);
+        // super.set_iteration_action(function(node: Node, relation: ChildRelation, child: Node, iterationValue){
+        //     if (child.getValue().length < searchstring.length){ 
+        //         return iterationValue.slice(child.getValue().length) 
+        //         }
+        //         return "";
+        //     })
+
+    }
 }
 
